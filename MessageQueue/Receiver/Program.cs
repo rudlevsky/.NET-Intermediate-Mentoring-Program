052fade070ddd0405eb40fea6a71c82e48fc191d
Ilya Rudlevsky <Ilya.Rudlevsky@thomsonreuters.com>
2021-11-09 18:21:01 +0300

    Added message queue taskk

---
+++ MessageQueue/Receiver/Program.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.Azure.ServiceBus;
+using Microsoft.Azure.ServiceBus.Management;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Receiver
+{
+	class Program
+	{
+		static string connectionString = "Endpoint=sb://mentoringqueue.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=nc3aLzU6Wvm3YDfUwgSRkiECQgyS97n3YxjNj+seeNg=";
+		static string queueName = "queue1";
+
+		static IQueueClient client;
+		static string outputPath = "C:\\Users\\Ilya_Rudlevsky\\Desktop\\Output";
+		static string currentFile = string.Empty;
+		static string currentFilePath = string.Empty;
+		static int bytesCount = 0;
+
+		static void Main(string[] args)
+		{
+			MainAsync().GetAwaiter().GetResult();
+		}
+
+		static async Task MainAsync()
+		{
+			var managementClient = new ManagementClient(connectionString);
+			var allQueues = await managementClient.GetQueuesAsync();
+			var foundQueue = allQueues.Where(q => q.Path == queueName.ToLower()).SingleOrDefault();
+
+			if (foundQueue == null)
+			{
+				await managementClient.CreateQueueAsync(queueName);
+			}
+
+			client = new QueueClient(connectionString, queueName, ReceiveMode.ReceiveAndDelete);
+
+			client.RegisterMessageHandler(ProcessMessagesAsync, new MessageHandlerOptions(ExceptionReceivedHandler)
+			{
+				MaxConcurrentCalls = 3,
+				AutoComplete = false
+			});
+
+			Console.ReadLine();
+			await client.CloseAsync();
+		}
+
+		static async Task ProcessMessagesAsync(Message message, CancellationToken token)
+		{
+			string fileName = message.Label;
+			
+			if (currentFile != fileName)
+			{
+				Console.WriteLine("Retrieving new file {0}", fileName);
+				currentFilePath = Path.Combine(outputPath, fileName);
+				currentFile = fileName;
+			}
+
+			var position = Convert.ToInt64(message.UserProperties["position"]);
+			var size = Convert.ToInt32(message.UserProperties["size"]);
+			var fileSize = Convert.ToInt64(message.UserProperties["fileSize"]);
+
+			using (var fileStream = new FileStream(currentFilePath, FileMode.OpenOrCreate, FileAccess.Write, FileShare.None))
+			{
+				fileStream.Position = position;
+				fileStream.Write(message.Body, 0, size);
+				fileStream.Flush();
+			}
+
+			bytesCount += size;
+			Console.WriteLine("Current size: {0}", bytesCount);
+
+			if (bytesCount == fileSize)
+			{
+				Console.WriteLine($"Received file {fileName} and saved it");
+				bytesCount = 0;
+			}
+
+			await client.CompleteAsync(message.SystemProperties.LockToken);
+		}
+
+		private static Task ExceptionReceivedHandler(ExceptionReceivedEventArgs arg)
+		{
+			return Task.FromResult(arg.Exception);
+		}
+	}
+}
---
+++ MessageQueue/Sender/Program.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.Azure.ServiceBus;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Sender
+{
+	class Program
+	{
+		static string connectionString = "Endpoint=sb://mentoringqueue.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=nc3aLzU6Wvm3YDfUwgSRkiECQgyS97n3YxjNj+seeNg=";
+		static string queueName = "queue1";
+
+		static IQueueClient client;
+
+		const int chunkSize = 256000;
+		static string inputPath = "C:\\Users\\Ilya_Rudlevsky\\Desktop\\Input";
+
+		static void Main(string[] args)
+		{
+			MainAsync().GetAwaiter().GetResult();
+		}
+
+		static async Task MainAsync()
+		{
+			var path = Path.Combine(inputPath);
+			var fileWatcher = new FileSystemWatcher(path);
+
+			fileWatcher.Created += SendMessageAsync;
+			fileWatcher.EnableRaisingEvents = true;
+
+			client = new QueueClient(connectionString, queueName);
+
+			Console.WriteLine("Press Enter to stop the service");
+
+			var input = Console.ReadKey();
+			if (input.Key == ConsoleKey.Enter)
+			{
+				await client.CloseAsync();
+			}
+		}
+
+		static async void SendMessageAsync(object sender, FileSystemEventArgs e)
+		{
+			try
+			{
+				int sentCount = 0;
+				Console.WriteLine($"Created: {e.FullPath}");
+
+				using (FileStream fileStream = File.OpenRead(e.FullPath))
+				{
+					while (fileStream.Position != fileStream.Length)
+					{
+						var buffer = new byte[chunkSize];
+
+						var message = new Message(buffer)
+						{
+							Label = e.Name
+						};
+
+						message.UserProperties.Add("position", fileStream.Position);
+
+						var readBytes = fileStream.Read(buffer, 0, chunkSize);
+
+						message.UserProperties.Add("size", readBytes);
+						message.UserProperties.Add("fileSize", fileStream.Length);
+
+						await client.SendAsync(message);
+
+						sentCount += readBytes;
+						Console.WriteLine("Sent: {0}", sentCount);
+					}
+				}
+
+				sentCount = 0;
+				Console.WriteLine($"File successfully sent");
+			}
+			catch(Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+		}
+	}
+}
